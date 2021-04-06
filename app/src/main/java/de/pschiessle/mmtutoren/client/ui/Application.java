d9422f0bc8b8335dcdff1ca4f2024ad07204a116
Pascal Schiessle <pascal.schiessle@web.de>
2021-04-06 13:16:58 +0200

    Sticky UI container

--- app/src/main/java/de/pschiessle/mmtutoren/client/ui/Application.java
+++ app/src/main/java/de/pschiessle/mmtutoren/client/ui/Application.java
@@ -8,16 +8,18 @@ import de.pschiessle.mmtutoren.client.ui.visualobj.VisualCamera;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Application extends JFrame {
 
+    public static final int BOTTOM_RIGHT_PANEL_WIDTH = 300;
+    public static final int BOTTOM_RIGHT_PANEL_HEIGHT = 150;
+
     private Networker networker;
 
-    public Application(){
+    public Application() {
         initRessources();
         initUI();
         initNetwork();
@@ -33,52 +35,57 @@ public class Application extends JFrame {
     }
 
     private void initUI() {
-        setLayout(new BorderLayout());
-        add(new Board(10,10, new VisualCamera(2, 0,0)), BorderLayout.CENTER);
 
-        CustomButton button = null;
-        try {
-            button = new CustomButton("hello",
-                    ImageIO.read(getClass().getResource("/images/sprite_floor_rock.png")),
-                    ImageIO.read(getClass().getResource("/images/sprite_floor_grass_0.png")));
-            button.setSize(100,50);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JLayeredPane layeredPane = new JLayeredPane();
+        JPanel boardPanel = new Board(10,10, new VisualCamera(2, 0,0));
+        JPanel uiPanel = new JPanel();
 
-        add(button, BorderLayout.SOUTH);
-        //TESTING ONLY
-        addKeyListener(new KeyListener() {
+        ComponentListener resizeListener = new ComponentAdapter() {
             @Override
-            public void keyTyped(KeyEvent e) {
-
-
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                boardPanel.setBounds(0,0, layeredPane.getWidth(), layeredPane.getHeight());
+                uiPanel.setBounds(
+                        layeredPane.getWidth() - BOTTOM_RIGHT_PANEL_WIDTH,
+                        layeredPane.getHeight() - BOTTOM_RIGHT_PANEL_HEIGHT,
+                        BOTTOM_RIGHT_PANEL_WIDTH,
+                        BOTTOM_RIGHT_PANEL_WIDTH);
+                System.out.println("RESIZE TO=[" + getWidth() + "," + getHeight() + "]");
             }
+        };
+        layeredPane.addComponentListener(resizeListener);
+        this.setPreferredSize(new Dimension(800, 400));
+        this.setLayout(new BorderLayout());
+        this.add(layeredPane, BorderLayout.CENTER);
+        layeredPane.setBounds(0, 0, 200,300);
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-                System.out.println("KEY=" + e.getKeyCode());
-                if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                    System.out.println("Sending message");
-                    networker.send("Hello World");
-                }
-            }
+        boardPanel.setBackground(Color.BLUE);
+        boardPanel.setBounds(0,0, layeredPane.getWidth(), layeredPane.getHeight());
+        boardPanel.setOpaque(true);
+        layeredPane.add(boardPanel, 0, 0);
 
-            @Override
-            public void keyReleased(KeyEvent e) {
 
-            }
-        });
+        uiPanel.setBackground(Color.GREEN);
+        uiPanel.setBounds(
+                layeredPane.getWidth() - BOTTOM_RIGHT_PANEL_WIDTH,
+                layeredPane.getHeight() - BOTTOM_RIGHT_PANEL_HEIGHT,
+                BOTTOM_RIGHT_PANEL_WIDTH,
+                BOTTOM_RIGHT_PANEL_WIDTH);
+        uiPanel.setOpaque(true);
+        layeredPane.add(uiPanel, 1, 0);
+        pack();
+        setVisible(true);
 
-        setSize(800,600);
         setTitle("Game me b***");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+
     }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
-            Application ex  = new Application();
+            Application ex = new Application();
             ex.setVisible(true);
         });
     }
